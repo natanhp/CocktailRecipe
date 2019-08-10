900a83aefe48178bd6b35a647bf40a1d0458f876
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-08-11 02:06:57 +0700

    Membuat kelas model untuk about me

---
+++ app/src/main/java/com/dicoding/submission/cocktailrecipe/Models/AboutMeModel.java
@@ -0,0 +1,10 @@
+package com.dicoding.submission.cocktailrecipe.Models;
+
+import lombok.Data;
+
+@Data
+public class AboutMeModel {
+    private String nama;
+    private String email;
+    private String urlPhoto;
+}
