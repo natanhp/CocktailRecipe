a2ed97833ecea0d6e8c685a9c2026cbd657e688f
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-08-10 15:45:02 +0700

    Membuat kelas repository untuk cocktail

---
+++ app/src/main/java/com/dicoding/submission/cocktailrecipe/Repositories/CoctailRepository.java
@@ -0,0 +1,65 @@
+package com.dicoding.submission.cocktailrecipe.Repositories;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.dicoding.submission.cocktailrecipe.Dao.CocktailDao;
+import com.dicoding.submission.cocktailrecipe.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class CoctailRepository implements CocktailDao {
+    private MutableLiveData<JSONObject> jsonObjectLiveData;
+
+    @SuppressLint("StaticFieldLeak")
+    private class JSONAksesAsynTask extends AsyncTask<Application, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(Application... applications) {
+            JSONObject jsonObject = null;
+
+            InputStream inputStream = applications[0].getApplicationContext().getResources().openRawResource(R.raw.Cocktails);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder stringBuilder = new StringBuilder();
+            try {
+                for (String line; (line = bufferedReader.readLine()) != null; ) {
+                    stringBuilder.append(line).append("\n");
+                }
+
+                String resultString = stringBuilder.toString();
+                JSONTokener jsonTokener = new JSONTokener(resultString);
+
+                jsonObject = new JSONObject(jsonTokener);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return jsonObject;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject object) {
+            super.onPostExecute(object);
+            jsonObjectLiveData.setValue(object);
+        }
+
+    }
+
+    @Override
+    public LiveData<JSONObject> creatJSONOBject(Application application) {
+        return jsonObjectLiveData;
+    }
+}
