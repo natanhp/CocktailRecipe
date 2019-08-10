13be90df733bcf24e42da9432438ff82102d0061
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-08-10 18:26:11 +0700

    Menghapus import yang tidak perlu

--- app/src/main/java/com/dicoding/submission/cocktailrecipe/Dao/CocktailDao.java
+++ app/src/main/java/com/dicoding/submission/cocktailrecipe/Dao/CocktailDao.java
@@ -1,12 +1,6 @@
 package com.dicoding.submission.cocktailrecipe.Dao;
 
-import android.app.Application;
-
-import androidx.lifecycle.LiveData;
-
 import org.json.JSONObject;
-
-import java.io.IOException;
 
 public interface CocktailDao {
     JSONObject creatJSONOBject();
