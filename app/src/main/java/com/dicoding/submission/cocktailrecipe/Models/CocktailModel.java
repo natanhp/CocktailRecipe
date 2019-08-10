2971bda58a52d432f9addb1639525cc0791fa646
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-08-11 00:13:43 +0700

    Membuat model menjadi parcelable

--- app/src/main/java/com/dicoding/submission/cocktailrecipe/Models/CocktailModel.java
+++ app/src/main/java/com/dicoding/submission/cocktailrecipe/Models/CocktailModel.java
@@ -1,13 +1,48 @@
 package com.dicoding.submission.cocktailrecipe.Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 import lombok.Data;
 
 @Data
-public class CocktailModel {
+public class CocktailModel implements Parcelable {
     private String name;
     private String UriImage;
     private List<String> ingredients;
     private List<String> howToMake;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.UriImage);
+        dest.writeStringList(this.ingredients);
+        dest.writeStringList(this.howToMake);
+    }
+
+    protected CocktailModel(Parcel in) {
+        this.name = in.readString();
+        this.UriImage = in.readString();
+        this.ingredients = in.createStringArrayList();
+        this.howToMake = in.createStringArrayList();
+    }
+
+    public static final Parcelable.Creator<CocktailModel> CREATOR = new Parcelable.Creator<CocktailModel>() {
+        @Override
+        public CocktailModel createFromParcel(Parcel source) {
+            return new CocktailModel(source);
+        }
+
+        @Override
+        public CocktailModel[] newArray(int size) {
+            return new CocktailModel[size];
+        }
+    };
 }
