a214cc509d75fb6506fe599c9c5abd5e62c6b2b1
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-08-28 08:43:34 +0700

    Membuat getter untuk memanggil asynctask dan state

--- app/src/main/java/com/dicoding/submission/cocktailrecipe/ViewModels/CocktailViewModel.java
+++ app/src/main/java/com/dicoding/submission/cocktailrecipe/ViewModels/CocktailViewModel.java
@@ -11,15 +11,25 @@ import com.dicoding.submission.cocktailrecipe.Repositories.CoctailRepository;
 
 import java.util.List;
 
+import lombok.Getter;
+
 public class CocktailViewModel extends AndroidViewModel {
     private CoctailRepository coctailRepository;
+
+    @Getter
+    private Boolean state;
 
     public CocktailViewModel(@NonNull Application application) {
         super(application);
         coctailRepository = new CoctailRepository(application);
+        state = coctailRepository.getState();
     }
 
     public LiveData<List<CocktailModel>> getDataCoctail(){
         return coctailRepository.getDataCoctail();
+    }
+
+    public void runThread(){
+        coctailRepository.runThread();
     }
 }
